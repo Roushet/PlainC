d082f6b0f4bbf60275519bff9d156855ccab15e1
Vladimir <vovanpdawan@gmail.com>
2018-02-06 15:46:39 +0300

    Закончил домашку

--- Lesson3Homework/Lesson3Homework/Program.c
+++ Lesson3Homework/Lesson3Homework/Program.c
@@ -1,11 +1,6 @@
-//1. Попробовать оптимизировать пузырьковую сортировку.Сравнить количество операций сравнения оптимизированной и 
-//не оптимизированной программы.Написать функции сортировки, которые возвращают количество операций.
-
-//3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив.Функция возвращает 
-//индекс найденного элемента или - 1, если элемент не найден.
-//4. *Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма.
-
 #define _CRT_SECURE_NO_WARNINGS
+#define ARR_SIZE 1000
+#define  ITERATIONS 1000
 
 #include <stdio.h>
 #include <math.h>
@@ -14,16 +9,21 @@
 #include <limits.h>
 #include <stdint.h>
 
+//Владимир Евдокимов
+//Задачу 3 (двоичный поиск - не делал, мы её сделали на уроке)
+
 //счётчик операций
 unsigned count;
 
 void Solution1();
 void Solution2();
+void Solution3();
 
 void BubbleSort(unsigned * array, const unsigned N);
 void Swap(unsigned* a1, unsigned* a2);
 unsigned Compare(unsigned a1, unsigned a2);
 void ShakerSort(unsigned* array, const unsigned N);
+void FillArray(unsigned* array, const unsigned N);
 
 int main() {
 
@@ -43,7 +43,7 @@ int main() {
 			Solution2();
 			break;
 		case 3:
-			//Solution3();
+			Solution3();
 			break;
 
 		default:
@@ -87,8 +87,11 @@ void BubbleSort(unsigned* array, const unsigned N)
 			}
 		}
 	}
+	//printf("COUNT %d \n", count);
+	//count = 0;
 }
 
+//функция сравнения элементов со счётчиком
 unsigned Compare(unsigned a, unsigned b) {
 
 	count++;
@@ -97,6 +100,7 @@ unsigned Compare(unsigned a, unsigned b) {
 	else return 0;
 }
 
+//функция обмена элементов со счётчиком
 void Swap(unsigned* a, unsigned* b) {
 	count++;
 
@@ -122,35 +126,107 @@ void Solution2() {
 void ShakerSort(unsigned* array, const unsigned N)
 {
 	unsigned i, j;
-	unsigned shaker = 1;
+	int changed;
+	int shaker = 1; // число для проверки кратности прохода
 
 	for (i = 0; i < N; i++)
 	{
-		//if (Compare(shaker % 2, 1)) {
+		changed = 0;
+
+		if (shaker % 2 == 1)
+		{
+			//слева направо
 			for (j = 0; j < N - 1; j++)
 			{
-
+				//если элемент слева больше элемента справа
 				if (Compare(array[j], array[j + 1])) {
-					Swap(array[j], array[j + 1]);
-
-					//temp = array[j];
-					//array[j] = array[j + 1];
-					//array[j + 1] = temp;
+					Swap(&array[j], &array[j + 1]);
+					changed = 1;
 				}
 			}
-		//}
-		//else {
-		//	for (j = N - 1; j > 0; j--)
-		//	{
-		//		if (Compare(array[j - 1], array[j])) {
-		//			Swap(array[j], array[j - 1]);
-
-		//			//temp = array[j];
-		//			//array[j] = array[j + 1];
-		//			//array[j + 1] = temp;
-		//		}
-		//	}
-		//}
+		}
+		else
+		{
+			//справа налево
+			for (j = N - 1; j >= 1; j--)
+			{
+				//если элемент справа меньше элемента слева
+				if (Compare(array[j - 1], array[j])) {
+					Swap(&array[j], &array[j - 1]);
+					changed = 1;
+				}
+			}
+		}
 		shaker++;
+
+		//если не было изменений - прервать цикл
+		if (changed = 0) break;
 	}
+
+	//printf("COUNT %d \n", count);
+	//count = 0;
+}
+
+//4. *Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма.
+
+void Solution3() {
+
+	unsigned array[ARR_SIZE];
+	clock_t startTime;
+	clock_t endTime;
+	unsigned i;
+	double averageBuble = 0;
+	double averageShaker = 0;
+
+
+	for (i = 0; i < ITERATIONS; i++) {
+
+		//Создаём большие массивы
+		//Заполняем их рандомами
+		FillArray(array, ARR_SIZE);
+
+		//сортируем пузырьком
+		startTime = clock();
+		BubbleSort(array, ARR_SIZE);
+		endTime = clock();
+
+		//считаем среднее время для пузырька
+		averageBuble += (double)(endTime - startTime) / CLOCKS_PER_SEC;
+
+		printf("Iteration %d\t %lf\t %d ", i, (double)(endTime - startTime) / CLOCKS_PER_SEC, count);
+		
+		count = 0;
+		FillArray(array, ARR_SIZE);
+
+
+		startTime = clock();
+		ShakerSort(array, ARR_SIZE);
+		endTime = clock();
+
+		averageShaker += (double)(endTime - startTime) / CLOCKS_PER_SEC;
+
+		printf("%lf\t %d \n", (double)(endTime - startTime) / CLOCKS_PER_SEC, count);
+		count = 0;
+	}
+
+	printf("Average buble sort time %lf \n", averageBuble / (double) ITERATIONS);
+	printf("Average shaker sort time %lf \n", averageShaker / (double)ITERATIONS);
+
+}
+
+//Ассимптотическая сложность N^2, при размере массива 1000, получается 1 000 000
+//Количество вызовов на сортировке массива с 1000 случайных элементов получается примерно 1 200 000
+
+//Почему то шейкерная сортировка не даёт особой выгоды, возможно, я не докодил что-то в ней. Возможно там после каждого 
+//второго прохода надо сужать интервал
+
+void FillArray(unsigned* array, const unsigned N) {
+	unsigned i = 0;
+	
+	srand(time(NULL));   
+
+	for (i = 0; i < N; i++) {
+		array[i] = rand();
+	}
+
 }
