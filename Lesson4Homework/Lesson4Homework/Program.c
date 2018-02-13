42edd37b3639206a0c4b8b1979e70dbcb6740318
Vladimir <vovanpdawan@gmail.com>
2018-02-13 17:32:02 +0300

    Сделал всё, но с ошибками. Прости госпеде 8(

--- Lesson4Homework/Lesson4Homework/Program.c
+++ Lesson4Homework/Lesson4Homework/Program.c
@@ -20,18 +20,11 @@ unsigned len(char * A, char * B);
 unsigned Matrix(char * A, char * B);
 unsigned MatrixMax(char * A, char * B);
 
-//1. *Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и нахождение количество маршрутов.
-//Например, карта:
-//3 3
-//1 1 1
-//0 1 0
-//0 1 0
+void Solution3();
+void MoveKnigth(int curPosX, int curPosY);
 
-//2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
 
-//3. ***Требуется обойти конём шахматную доску размером NxM, пройдя через все поля доски по одному 
-// разу.Здесь алгоритм решения такой же как и в задаче о 8 ферзях.Разница только в проверке положения коня.
-
+//Владимир Евдокимов
 
 int main() {
 
@@ -51,7 +44,7 @@ int main() {
 			Solution2();
 			break;
 		case 3:
-			//Solution3();
+			Solution3();
 			break;
 
 		default:
@@ -72,8 +65,14 @@ unsigned field[SIZE][SIZE] = {
 };
 
 
+//1. *Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и нахождение количество маршрутов.
+//Например, карта:
+//3 3
+//1 1 1
+//0 1 0
+//0 1 0
+
 void Solution1() {
-	//5
 	printf("Ways count %d\n", WayCount(0, 0));
 
 }
@@ -82,6 +81,7 @@ unsigned WayCount(unsigned x, unsigned y) {
 	if (x == 5 && y == 5)
 		return 1;
 
+	//проверка на препятствие
 	if (field[x][y] == 1)
 		return 0;
 
@@ -91,29 +91,24 @@ unsigned WayCount(unsigned x, unsigned y) {
 	return WayCount(x, y + 1) + WayCount(x + 1, y);
 }
 
+
+//2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+
 void Solution2() {
 
 	unsigned i = 0;
 	char *str1 = "geekbrains";
 	char *str2 = "geekminds";
 
-	//for (i = 0; i < 20; i++) {
-	//	printf("%c \n", *(str1 + i));
-	//	
-	//	if (*(str1 + i) == '\0') break;
-	//}
 
-	//do {
-	//	i++;
-	//	printf("%c", *(str1 + i));
-	//} while (*(str1 + i) != '\0');
+	
+	printf("LCS matrix: %d \n", Matrix(str1, str2));
 
-	//printf("%d \n", i);
-
-	Matrix(str1, str2);
-
-	//printf("%d \n", len(str1, str2));
+	printf("LCS recurcive %d \n", len(str1, str2));
 }
+
+//решение через матрицу
+//вероятно тут ошибка в подсчётах НОС
 
 unsigned Matrix(char *str1, char *str2) {
 
@@ -122,7 +117,6 @@ unsigned Matrix(char *str1, char *str2) {
 	unsigned j = 0;
 
 	unsigned lcs = 0;
-	unsigned counter = 0;
 
 	if (*str1 == '\0' || *str2 == '\0')
 		return 0;
@@ -137,13 +131,10 @@ unsigned Matrix(char *str1, char *str2) {
 	for (i = 0; i < len1; i++)
 		mat[i] = malloc(sizeof(unsigned)*len2); //столбцы
 
-	//заполняем матрицу
-	//https://foxford.ru/wiki/informatika/naibolshaya-obschaya-podposledovatelnost
-
+	//заполняем матрицу нулями
 	for (i = 0; i < len1; i++) {
 		for (j = 0; j < len2; j++) {
 			mat[i][j] = 0;
-			//printf("%d  ", mat[i][j]);
 		}
 	}
 
@@ -159,31 +150,20 @@ unsigned Matrix(char *str1, char *str2) {
 
 	for (i = 0; i < len1; i++) {
 		for (j = 0; j < len2; j++) {
-			//
-			//if (i == 0 || j == 0) {
-			//	mat[j][i] = 0;
-			//	continue;
-			//}
-			//если буква массива 1 = буква массива 2 прибавляем счётчик
-
-
+			//Если буквы равны
 			if (*(str1 + i) == *(str2 + j)) {
+				//если это первая строка
 				if (i == 0) mat[i][j] = 1;
-
+				//если не первая строка, то учитываем результат прошлой строки
 				if (i > 0)
-					mat[i][j] = 1 + mat[i - 1][j];
+					mat[i][j] = 1; // +mat[i - 1][j];
+				//если поставили значение в клетку - переходим к следующему витку цикла
+				continue;
 			}
-			else {
-				mat[i][j] = 0;
-			}
-
-			if (j > 0) {
-				mat[i][j] = mat[i][j - 1];
-			}
-
 		}
 	}
 
+	//Печать матрицы
 	for (i = 0; i < len1; i++) {
 		for (j = 0; j < len2; j++) {
 			printf("%d \t ", mat[i][j]);
@@ -191,7 +171,21 @@ unsigned Matrix(char *str1, char *str2) {
 		printf("\n");
 	}
 
+	//Длина наибольшей общей подпоследовательности = сумма измененных столбцов
+	//Проходим по столбцам
+	for (j = 0; j < len2; j++) {
+		//по строкам
+		for (i = 0; i < len1; i++)
+		{
+			if (mat[i][j] > 0)
+			{
+				lcs++;
+				break; //при первом вхождении считаем что есть общая буква и прерываем цикл
+			}
+		}
+	}
 
+	return lcs;
 }
 
 //длина строки - нужна чтобы создать матрицу правильного размера
@@ -204,6 +198,7 @@ unsigned strLen(char *str) {
 	return i;
 }
 
+//решение с урока через рекурсию
 unsigned len(char * A, char * B)
 {
 	if (*A == '\0' || *B == '\0')
@@ -220,4 +215,85 @@ unsigned len(char * A, char * B)
 
 unsigned MatrixMax(char * A, char * B) {
 	return 1;
+}
+
+//3. ***Требуется обойти конём шахматную доску размером NxM, пройдя через все поля доски по одному 
+// разу.Здесь алгоритм решения такой же как и в задаче о 8 ферзях.Разница только в проверке положения коня.
+
+unsigned Chessfield[8][8] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+unsigned movesX[8] = { 2, 1, -1, -2, -2, -1,  1,  2 };
+unsigned movesY[8] = { 1, 2,  2,  1, -1, -2, -2, -1 };
+
+//счётчик ходов
+//наверное не самое лучшее решение делать счётчик для рекурсии вне рекурсии
+unsigned move = 1;
+
+void Solution3() {
+	// печать поля
+	unsigned i, j;
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			printf("%d \t ", Chessfield[i][j]);
+		}
+		printf("\n");
+	}
+	//начинаем движение с точки 0 0, ход 1
+	MoveKnigth(0, 0);
+	
+	printf("\n");
+
+	// печать поля
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			printf("%d \t ", Chessfield[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+
+void MoveKnigth(int curPosX, int curPosY) {
+	int i = 0;
+
+	//Конь пришёл в клетку и метит её как посещённую
+	Chessfield[curPosX][curPosY] = move;
+	move++;
+
+	//определяем клетку цель
+	for (i = 0; i < 8; i++) {
+		//printf("X %d Y %d \n", curPosX + movesX[i], curPosY + movesY[i]);
+
+		//если клетка в пределах поля - ошибка
+		if (curPosX + movesX[i] < 0 || 
+			curPosY + movesY[i] < 0 || 
+			curPosX + movesX[i]> 7 || 
+			curPosY + movesY[i] > 7) 
+		{
+			return;
+		}
+
+		//если клетка не была посещена - инвариант
+		if (Chessfield[curPosX + movesX[i]][curPosY + movesY[i]] == 1) {
+			return;
+		}
+
+		//если клетка не была посещена - наша цель
+		if (Chessfield[curPosX + movesX[i]][curPosY + movesY[i]] == 0) {
+			//тогда перейти в клетку
+			MoveKnigth(curPosX + movesX[i], curPosY + movesY[i]);
+		}
+	}
+	//когда все возможности исчерпаны
+	return;
 }
