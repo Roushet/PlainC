2778132431773fd46818e241d21601d400abbd46
Vladimir <vovanpdawan@gmail.com>
2018-02-19 19:14:03 +0300

    сделал задание про скобки

--- Lesson5Homework/Lesson5Homework/Program.c
+++ Lesson5Homework/Lesson5Homework/Program.c
@@ -9,12 +9,20 @@
 
 void Solution1();
 
-void stack_push(int var);
-int stack_pop();
+void array_stack_push(int var);
+int array_stack_pop();
 
 //void Solution2();
-//void Solution3();
+void Solution3();
 
+char * stack_create(const unsigned size);
+void stack_destroy(char *stack);
+void stack_push(char *stack, char value);
+char stack_pop(char *stack);
+
+int IsOpenBracket(char c);
+int IsCloseBracket(char c);
+char getReverceBracket(char c);
 
 //Владимир Евдокимов
 
@@ -53,7 +61,7 @@ int main() {
 			//Solution2();
 			break;
 		case 3:
-			//Solution3();
+			Solution3();
 			break;
 
 		default:
@@ -77,14 +85,14 @@ void Solution1() {
 
 	//заполняем стек
 	do {
-		stack_push(num % 2);
+		array_stack_push(num % 2);
 		//printf("%d", num % 2);
 		num /= 2;
 	} while (num != 0);
 
 	//очищаем стек
 	do {
-		printf("%d", stack_pop());
+		printf("%d", array_stack_pop());
 	} while (Cursor >= 0);
 	printf("\n");
 
@@ -92,7 +100,7 @@ void Solution1() {
 
 //Стек
 
-void stack_push(int var) {
+void array_stack_push(int var) {
 
 	if (Cursor > 100 - 1) {
 		printf("Stack is full, cannot add new element\n");
@@ -103,7 +111,7 @@ void stack_push(int var) {
 	Stack[Cursor] = var;
 }
 
-int stack_pop() {
+int array_stack_pop() {
 	int temp;
 
 	if (Cursor < 0) {
@@ -116,32 +124,120 @@ int stack_pop() {
 	return temp;
 }
 
-int * stack_create(const unsigned size);
-void stack_destroy(int *stack);
-void stack_push(int *stack, const int value);
-int stack_pop(int *stack);
 
-int * stack_create(const unsigned size) {
-	int *p = (int *)malloc(sizeof(int) * (size + 1));
+char * stack_create(const unsigned size) {
+	char *p = (char *)malloc(sizeof(char) * (size + 1));
 	p[0] = 1;
 	return p;
 }
 
-void stack_destroy(int *stack) {
+void stack_destroy(char *stack) {
 	free(stack);
 }
 
-void stack_push(int *stack, const int value) {
+void stack_push(char *stack, char value) {
 	stack[stack[0]] = value;
 	stack[0]++;
 }
 
-int stack_pop(int *stack) {
+char stack_pop(char *stack) {
 	stack[0]--;
-	int value = stack[stack[0]];
+	char value = stack[stack[0]];
 	return value;
 }
 
-void Solution2() {
+//3. Написать программу, которая определяет, является ли введенная скобочная последовательность 
+//правильной.Примеры правильных скобочных выражений : (), ([])(), {}(), ([{}]), неправильных — )(, ())({ ), (, ]) }), ([(]) для скобок[, (, { .
+//Например: (2 + (2 * 2)) или[2 / {5 * (4 + 7)}]
+void Solution3() {
 
+	char string[50];
+	char *char_stack = stack_create(50);
+
+
+	printf("please input formula: \n");
+
+	scanf("%s", &string);
+
+
+
+	unsigned i = 0;
+	printf("FORMULA: ");
+
+	do {
+		printf("%c", *(string + i));
+
+		i++;
+	} while (*(string + i) != '\0');
+
+	i = 0;
+
+	do {
+		printf("%с", *(string + i));
+
+		if (IsOpenBracket(*(string + i)))
+			stack_push(char_stack, *(string + i));
+
+		if (IsCloseBracket(*(string + i))) {
+			if (getReverceBracket(*(string + i)) != stack_pop(char_stack))
+			{
+				printf("Brackets does not comply. Fail.\n");
+				break;
+			}
+		}
+		i++;
+
+		//сделать ок для формулы
+	} while (*(string + i) != '\0');
+
+	printf("\n");
+
+
+
+}
+int IsOpenBracket(char c) {
+	char *openbrackets = "{[(";
+	unsigned i = 0;
+
+	do {
+		if (c == openbrackets[i])
+			return 1;
+		i++;
+	} while (*(openbrackets + i) != '\0');
+
+	return 0;
+}
+
+int IsCloseBracket(char c) {
+	char *closebrackets = "}])";
+	unsigned i = 0;
+
+	do {
+		if (c == closebrackets[i])
+			return 1;
+		i++;
+	} while (*(closebrackets + i) != '\0');
+
+	return 0;
+}
+
+//закрывающая на открывающую
+//приходит закрывающая
+char getReverceBracket(char c) {
+	char *openbrackets = "{[(";
+	char *closebrackets = "}])";
+
+	printf("\n");
+
+	unsigned i = 0;
+	do {
+		printf("%c vs ", c);
+		printf("%c", *(closebrackets + i));
+
+		if (c == (*(closebrackets + i)))
+			return (*(openbrackets + i));
+		i++;
+	} while (*(closebrackets + i) != '\0');
+
+	return '\0';
 }
